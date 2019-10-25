4168f470f37782c0cd9f3819213c0814698ecdd1
lm2612 <lm2612@ic.ac.uk>
2019-10-25 12:06:25 +0100

    more recent file version updated

--- Inputs/ImportData.py
+++ Inputs/ImportData.py
@@ -6,7 +6,7 @@ from ReadAvgFiletyr import *
 datadir = GetDir()
 time = 5
 
-filename = datadir+'All_input_Allfixed_%syr.nc'%time
+filename = datadir+'All_input_Allfixed_%syrALL.nc'%time
 (X_SfcTemp,X_AirTemp500,X_GeoHeight500,X_SLP,X_RF,y,
  lons,lats,lons1,lats1,Names) = OpenFile(filename)
 print((lons,lats))
