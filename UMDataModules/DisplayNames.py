357b9e4de8877a6c94e3afea9cd2cf367814eb1a
lm2612 <lm2612@ic.ac.uk>
2019-11-25 12:47:18 +0000

    display names added to convert original file names to file names used in paper

---
+++ UMDataModules/DisplayNames.py
@@ -0,0 +1,30 @@
+# Dictionary to convert full names into names with underscores and small x instead of 
+# big X in 10x, 2x, etc. for final paper, as decided with Apostolos on Fri 11th Oct 2019
+
+display_names = {'No SO2 China':'No_SO2_China',
+             'No SO2 Europe': 'No_SO2_Europe' ,
+             'No SO2 India': 'No_SO2_India',
+             'No SO2 US':'No_SO2_US',
+             'No SO2 East Asia':'No_SO2_East_Asia',
+             'No SO2 NHML':'No_SO2_NHML',
+             'No BC NHML':'No_BC_NHML',
+  	     'No Dust Arabia':'No_Dust_Arabia',
+             '5X SO4':'5xSO4_Global',
+             '10X BC':'10xBC_Global',
+             '2X CO2':'2xCO2_PDRMIP',
+             '3X CH4':'3xCH4',
+             '10X Europe SO4':'10xSO4_Europe',
+             '10X Asia SO4':'10xSO4_Asia',
+             'pre-industrial SO4':'SO4_pre-industrial',
+             '10X Asia BC':'10xBC_Asia',
+             '2% Solar Constant increase':'+2%_Solar_Constant',
+             '10X CFC-12':'10xCFC-12',
+	     'No BC Global':'No_BC_Global',
+	     'No OC Global':'No_OC_Global',
+	     'No SO2 Global':'No_SO2_Global',	
+	     '2xCO2 Global':'2xCO2_ECLIPSE',
+             'No NOX Global':'No_NOX_Global',
+	     'No CO Global':'No_CO_Global',
+	     'No VOC Global':'No_VOC_Global',
+	     'No CH4 Global':'-20%_CH4' }
+
