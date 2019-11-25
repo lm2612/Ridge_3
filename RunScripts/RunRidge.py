8ddf9d40d1f4ff136a5843178853992c29eaeadf
lm2612 <lm2612@ic.ac.uk>
2019-11-25 12:43:35 +0000

    updated to run with most recent files

--- RunScripts/RunRidge.py
+++ RunScripts/RunRidge.py
@@ -21,26 +21,31 @@ import copy as cp
 from Names import *
 
 
-print(Names)
-Noisy = ['No Dust Arabia','No VOC Global','No SO2 India','No OC Global','No NOX Global']
+datadir = GetDir()
+time = 10
+
+filename = datadir+'Inputs_0-{}_yrs_Outputs_50-80_yrs.nc'.format(time)
+(X_SfcTemp,X_AirTemp500,X_GeoHeight500,X_SLP,X_RF,y,
+                lons,lats,lons1,lats1,Names) = OpenFile(filename)
+Noisy = ['No OC Global', 'No Dust Arabia','No VOC Global','No SO2 India','No NOX Global','No BC Europe','No BC East Asia','No BC US']
 for runname in Noisy:
        (Names,[X_SfcTemp,X_GeoHeight500,X_RF,y]) = remove_ind(Names,runname,[X_SfcTemp,X_GeoHeight500,X_RF,y] )
-# What is the predictor X?
+
 X = X_SfcTemp
 X_name = 'SfcTemp'
 
 
-# Any dimension reduction (eg PCA) or full grid?
+# Any dimension reduction (PCA, Regional or Full)?
 X_type = 'Full'
 y_type = 'Full'
 
 
-# alpha for regularisation, no of CV
+# Cross validation: alphas for regularisation, no of Cross validation folds
 alpha_list = np.logspace(-3,8,10)
 no_of_cv = 3
 
 # where to save?
-savedir = home+'/OutputsRidge/'
+savedir = home+'/WORK/Aligned_50-80/'
 save_filename = 'X={}_{},y={}_'.format(X_name,X_type,y_type)
 
 # regions of interest for metrics?
@@ -56,7 +61,7 @@ def run_one(data,name,output):
     # make a copy so we don't overwrite
     d = cp.copy(data)
     d.re_initialise()
-    d.run(X_type,y_type,'Ridge',name,cvfolds=no_of_cv,alpha_list=alpha_list, coefs=True, repeat_regularise=True, n_jobs=4)
+    d.run(X_type,y_type,'Ridge',name,cvfolds=no_of_cv,alpha_list=alpha_list, coefs=True, repeat_regularise=True, n_jobs=1)
     saveas = savedir+save_filename+name+'_'
     d.metrics_regional(saveas,regions_all)
     d.plot_results(savedir)
